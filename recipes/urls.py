693abad8959904f99e7eddc4e6de6915ca6edfb4
LeandroMelloo <leandromoreno1909@hotmail.com>
2022-04-15 14:39:07 -0300

    deletando arquivo desnecessarios

--- recipes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from recipes.views import about, contact, home
-
-urlpatterns = [
-    path('', home),  # dominio/recipes/
-    path('contact/', contact),  # dominio/recipes/contact/
-    path('about/', about),  # dominio/recipes/about/
-]
