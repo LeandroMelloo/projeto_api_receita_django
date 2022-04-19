03127570007d675d74cc5f3aea23afa6b82b4d5c
LeandroMelloo <leandromoreno1909@hotmail.com>
2022-04-19 20:25:05 -0300

    criando testes para criação do usuario e super usuario

--- app/main/tests/test_models.py
+++ app/main/tests/test_models.py
@@ -25,4 +25,15 @@ class ModelTests(TestCase):
 
     def test_new_user_invalid_email(self):
         """Test creating user with no email raises error"""
-        pass
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'leandromello0910@gmail.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
