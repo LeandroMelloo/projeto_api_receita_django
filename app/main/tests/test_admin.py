51b053d23c3bf5fa5c486f9ead42ea96169acce2
LeandroMelloo <leandromoreno1909@hotmail.com>
2022-06-01 09:56:44 -0300

    inserindo testes unitarios

---
+++ app/main/tests/test_admin.py
@@ -0,0 +1,11 @@
+"""
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+"""
+from django.test import TestCase
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        pass
