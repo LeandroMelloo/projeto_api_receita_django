e64532738a70ecfa47ad2ba171d49cf657733c47
LeandroMelloo <leandromoreno1909@hotmail.com>
2022-07-05 18:34:15 -0300

    Criando a documentação do projeto

--- app/core/admin.py
+++ app/core/admin.py
@@ -15,7 +15,14 @@ class UserAdmin(BaseUserAdmin):
     """
 
     ordering = ['id']
-    list_display = ['id', 'email', 'name']
+    list_display = [
+        'id',
+        'email',
+        'name',
+        'is_active',
+        'is_staff',
+        'is_superuser'
+    ]
     fieldsets = (
         (None, {'fields': ('email', 'name', 'password')}),
         (
@@ -31,6 +38,20 @@ class UserAdmin(BaseUserAdmin):
         (_('Data de acesso'), {'fields': ('last_login',)}),
     )
     readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
